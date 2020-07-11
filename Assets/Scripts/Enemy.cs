d8160e9a607554f61408e9e95e2ee58abedacb33
Michael Smith <silentcoder99@gmail.com>
2020-07-11 14:59:53 +1000

    Add perfectly normal enemies

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Vector3 direction;
+    private Rigidbody body;
+
+    public float speed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = transform.right;
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        body.AddForce(direction * speed);
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        direction = -direction;
+        Debug.Log("AHHHH");
+    }
+}
